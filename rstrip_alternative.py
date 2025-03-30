73a067a9c3a552f36ec8dc2ccd5d1f16bd3dee7f
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-31 01:20:01 +0800

    added initial pseudocode for program 1 batch 7

---
+++ rstrip_alternative.py
@@ -0,0 +1,7 @@
+#define rstrip_alt(str)
+#initialize string_len
+#create a for loop to iterate through string from end to start
+#within for loop, create an if statement to evaluate whether i is a whitespace
+#if above statement is True, replace i with none 
+#elif i is an alphabet, break loop
+#outside for loop, return str
