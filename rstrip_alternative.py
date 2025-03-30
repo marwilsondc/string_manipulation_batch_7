047ce44e8b2abf1003f3b3da86c994ef2991bd15
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-31 01:25:28 +0800

    added initial code for program 1 batch 7

--- rstrip_alternative.py
+++ rstrip_alternative.py
@@ -1,7 +1,24 @@
 #define rstrip_alt(str)
-#initialize string_len
-#create a for loop to iterate through string from end to start
-#within for loop, create an if statement to evaluate whether i is a whitespace
-#if above statement is True, replace i with none 
-#elif i is an alphabet, break loop
-#outside for loop, return str
+def rstrip_alt(string):
+
+    #initialize string_len
+    str_len = len(string)
+
+    #create a for loop to iterate through string from end to start
+    for i in string[str_len::-1]:
+
+        #within for loop, create an if statement to evaluate whether i is a whitespace
+        if i.isspace():
+
+            #if above statement is True, replace i with none
+            string = string.replace(i, "", -1)
+
+        #elif i is an alphabet or any other symbol, break loop
+        else:
+            break
+
+    #outside for loop, return str
+    return string
+
+user_string = input("input a string with several spaces after text: ")
+print(rstrip_alt(user_string))
