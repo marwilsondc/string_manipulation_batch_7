aaedbb2b020b1833113381304f9fabdfd48f155a
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-31 01:51:24 +0800

    fixed bug: tweaked for loop, if statements, and processes after

--- rstrip_alternative.py
+++ rstrip_alternative.py
@@ -1,21 +1,26 @@
 #define rstrip_alt(str)
 def rstrip_alt(string):
 
-    #initialize string_len
+    #initialize string_len and new_string
     str_len = len(string)
+    left_whitespace = ""
 
-    #create a for loop to iterate through string from end to start
-    for i in string[str_len::-1]:
+    #create a for loop to iterate through string from start to end
+    for i in string:
 
         #within for loop, create an if statement to evaluate whether i is a whitespace
         if i.isspace():
 
-            #if above statement is True, replace i with none
-            string = string.replace(i, "", -1)
+            #if above statement is True, add i to left_whitespace
+            left_whitespace += i
 
         #elif i is an alphabet or any other symbol, break loop
         else:
             break
+
+    #strip the string and combine with left_whitespace
+    string = string.strip()
+    string = left_whitespace + string
 
     #outside for loop, return str
     return string
