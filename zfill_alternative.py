2763c3ea2d0ab69bb25a7bb2108a844591a20688
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-31 20:25:09 +0800

    added initial code for program 7 batch 7

--- zfill_alternative.py
+++ zfill_alternative.py
@@ -1,5 +1,15 @@
 #define zfill_alt(string: str, len: int) -> str: 
-#initialize new_string = string
-#create while loop to compare len(new_string) with len
-#inside while loop, add "0" to new_string
-#outside while loop, return new_string
+def zfill_alt(string: str, length: int) -> str:
+
+    #initialize new_string = string
+    new_string = string
+
+    #create while loop to compare len(new_string) with len
+    while len(new_string) < length:
+
+        #inside while loop, add "0" to new_string
+        new_string = "0" + new_string
+
+    #outside while loop, return new_string
+    return new_string
+
