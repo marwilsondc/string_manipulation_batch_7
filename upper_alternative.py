7cb9547456ee1c001493106f27898ef19b2bd12b
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-31 18:41:50 +0800

    added initial code for program 3 batch 7

--- upper_alternative.py
+++ upper_alternative.py
@@ -1,7 +1,21 @@
 #define upper_alt(string: str) -> str:
-#within function, initialize new_string
-#then, create a for loop to iterate through string
-#within for loop, create an if statement to evaluate whether i is lowercase
-#if above statement is True, add i to new_string in uppercase through swapcase
-#else (uppercase, symbol, or whitespace), add i to new_string
-#outside for loop, return new_string
+def upper_alt(string: str) -> str:
+
+    #within function, initialize new_string
+    new_string = ""
+
+    #then, create a for loop to iterate through string
+    for i in string:
+
+        #within for loop, create an if statement to evaluate whether i is lowercase
+        if i.islower():
+
+            #if above statement is True, add i to new_string in uppercase through swapcase
+            new_string += i.swapcase()
+
+        #else (uppercase, symbol, or whitespace), add i to new_string
+        else:
+            new_string += i
+
+    #outside for loop, return new_string
+    return new_string
