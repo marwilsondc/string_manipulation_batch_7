8e73711259ba2f7630519d34c0ab57c2d9f2dec8
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-31 18:39:36 +0800

    added initial pseudocode for program 3 batch 7

---
+++ upper_alternative.py
@@ -0,0 +1,7 @@
+#define upper_alt(string: str) -> str:
+#within function, initialize new_string
+#then, create a for loop to iterate through string
+#within for loop, create an if statement to evaluate whether i is lowercase
+#if above statement is True, add i to new_string in uppercase through swapcase
+#else (uppercase, symbol, or whitespace), add i to new_string
+#outside for loop, return new_string
