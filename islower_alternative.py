20571acbe8d9534a6ebcbf49b5a2807b25184836
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-31 18:50:04 +0800

    uploaded islower_alternative.py and added initial pseudocode

---
+++ islower_alternative.py
@@ -0,0 +1,5 @@
+#define islower_alt(string: str) -> bool:
+#initialize str_comparator
+#create if statement to evaluate whether string is the same as str_comparator
+#if above statement is True, return True
+#else, return False
