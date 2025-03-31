4d41f0d1f804725d62dea49d76d5641f3871437e
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-31 18:51:39 +0800

    added initial code for program 4 batch 7

--- islower_alternative.py
+++ islower_alternative.py
@@ -1,5 +1,17 @@
 #define islower_alt(string: str) -> bool:
-#initialize str_comparator
-#create if statement to evaluate whether string is the same as str_comparator
-#if above statement is True, return True
-#else, return False
+def islower_alt(string: str) -> bool:
+
+    #initialize str_comparator
+    str_comparator = string.lower()
+
+    #create if statement to evaluate whether string is the same as str_comparator
+    if string == str_comparator:
+        
+        #if above statement is True, return True
+        return True
+    
+    #else, return False
+    else:
+        return False
+    
+
