327f2716f7e2839f4b531973ff952325360d9d5e
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-31 19:58:09 +0800

    added initial pseudocode to program 6 batch 7

---
+++ rjust_alternative.py
@@ -0,0 +1,5 @@
+#define rjust_alt(string: str, val: int, char: str = " ") -> str:
+#initialize new_string == string, new_string_len = len(new_string)
+#create while loop to evaluate new_string_len
+#within while loop, add whitespaces to new_string
+#outside while loop, return new_string
