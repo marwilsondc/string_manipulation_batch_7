32ba6debab72dd89f62f2f208ea09ca6e1cdccf2
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-31 19:15:21 +0800

    uploaded startswith_alternative.py and added initial pseudocode

---
+++ startswith_alternative.py
@@ -0,0 +1,8 @@
+#define startswith_alt(string: str, val: str, start: int = 0, end: int = len(string)) -> str:
+#initialize index_count, comparator, value_len
+#create for loop to iterate through string according to parameters
+#within for loop, create if statement to evaluate whether index_count is equal to value_len and comparator is equal to value
+#if above statement is True, return True
+#elif, evaluate if index_count is <= value_len and i is in value
+#if above statement is True, add i to comparator and continue
+#else, return False
