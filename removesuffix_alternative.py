e00995d19bab5e691337cec6946c88978cea12be
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-31 18:26:59 +0800

    fixed bug (program 2 batch 7): enhanced code and removed some initializations

--- removesuffix_alternative.py
+++ removesuffix_alternative.py
@@ -1,47 +1,36 @@
 #define removesuffix_alt(string: str, suffix: str) -> str:
 def removesuffix_alt(string: str, suffix: str) -> str:
 
-    #initialize suffix_len, compare_suffix, new_string, index_count, and done_flag
+    #initialize suffix_len, string_len, compare_suffix, remaining_char, index_count, and done_flag
     suffix_len = len(suffix)
     string_len = len(string)
+    remaining_char = string_len - suffix_len
     compare_suffix = ""
-    new_string = ""
     index_count = 0
-    done_flag = False
 
     #create a for loop to iterate through string from end to start
     for i in string[string_len::-1]:
 
+        #within initial if statement, create another if statement that evaluates if index_count is greater than len(suffix) and compares compare_suffix and suffix
+        if index_count == suffix_len and compare_suffix == suffix:
+                
+            #if above statement is True, return all
+            return(string[:remaining_char:1])
+            
         #within for loop, create if statement that evaluates whether i is alpha
-        if i.isalpha():
+        elif index_count <= suffix_len and i in suffix:
 
-            #if above statement is True, add i to compare suffix
+            #if initial if statement is True, add i to compare suffix
             compare_suffix = i + compare_suffix
 
-            #within initial if statement, create another if statement that evaluates if index_count is greater than len(suffix) and compares compare_suffix and suffix
-            if index_count > suffix_len and compare_suffix == suffix:
-                
-                #if above statement is True, add all remaining characters to new_string
-                done_flag = True
-                new_string += i
-                continue 
-            
             #then add 1 to index_count
             index_count += 1
         
-        #outside initial if statement, continue elif: evaluate whether done_flag is True, then add i to new_string (for whitespaces)
-        elif done_flag:
-            new_string += i
-            continue
-        
         #outside initial if statement, continue to elif: to evaluate whether first iteration is a space or symbol AND whether done_flag false
-        elif done_flag == False:
+        else:
 
             #if above elif is True, return string
             return string
-        
-#return new_string
-    return new_string
 
 #tester
 user_string = input("input a string: ")
