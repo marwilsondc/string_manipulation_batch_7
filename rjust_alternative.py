beb0a0ea9f09ad5b521d36dcecbf9bae62826070
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-31 20:08:22 +0800

    added tester to program 6 batch 7 and encountered bug: endless loop

--- rjust_alternative.py
+++ rjust_alternative.py
@@ -5,11 +5,19 @@ def rjust_alt(string: str, val: int, char: str = " ") -> str:
     new_string = string
     new_string_len = len(new_string)
 
-    #create while loop to evaluate new_string_len
+    #create while loop to evaluate 
+    #bug: endless loop
     while new_string_len < val:
 
         #within while loop, add whitespaces to new_string
-        new_string = " " + new_string
+        new_string = char + new_string
 
     #outside while loop, return new_string
     return new_string
+
+#tester
+user_string = input("input a string to use rjust on: ")
+user_val = int(input("input a value for padding: "))
+
+print(rjust_alt(user_string, user_val))
+print(rjust_alt(user_string, user_val, "0"))
