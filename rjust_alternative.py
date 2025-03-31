985f4fd1c550ae412b83032d5930f839296590ba
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-31 20:01:04 +0800

    added initial code to program 6 batch 7

--- rjust_alternative.py
+++ rjust_alternative.py
@@ -1,5 +1,15 @@
 #define rjust_alt(string: str, val: int, char: str = " ") -> str:
-#initialize new_string == string, new_string_len = len(new_string)
-#create while loop to evaluate new_string_len
-#within while loop, add whitespaces to new_string
-#outside while loop, return new_string
+def rjust_alt(string: str, val: int, char: str = " ") -> str:
+
+    #initialize new_string == string, new_string_len = len(new_string)
+    new_string = string
+    new_string_len = len(new_string)
+
+    #create while loop to evaluate new_string_len
+    while new_string_len < val:
+
+        #within while loop, add whitespaces to new_string
+        new_string = " " + new_string
+
+    #outside while loop, return new_string
+    return new_string
