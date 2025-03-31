d013363af0c506df0693b587d94851c5886c2c01
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-31 19:22:58 +0800

    added initial code for program 5 batch 7

--- startswith_alternative.py
+++ startswith_alternative.py
@@ -1,8 +1,27 @@
 #define startswith_alt(string: str, val: str, start: int = 0, end: int = len(string)) -> str:
-#initialize index_count, comparator, value_len
-#create for loop to iterate through string according to parameters
-#within for loop, create if statement to evaluate whether index_count is equal to value_len and comparator is equal to value
-#if above statement is True, return True
-#elif, evaluate if index_count is <= value_len and i is in value
-#if above statement is True, add i to comparator and continue
-#else, return False
+def startswith_alt(string: str, val: str, start: int = 0, end: int = len):
+
+    #initialize index_count, comparator, value_len
+    index_count = 0
+    comparator = ""
+    value_len = len(val)
+
+    #create for loop to iterate through string according to parameters
+    for i in string[start:end:1]:
+
+        #within for loop, create if statement to evaluate whether index_count is equal to value_len and comparator is equal to value
+        if index_count == value_len and comparator == val:
+
+            #if above statement is True, return True
+            return True
+        
+        #elif, evaluate if index_count is <= value_len and i is in value
+        elif index_count <= value_len and i in val:
+
+            #if above statement is True, add i to comparator, add 1 to index_count, and continue
+            comparator += i
+            index_count += 1
+
+        #else, return False
+        else:
+            return False
