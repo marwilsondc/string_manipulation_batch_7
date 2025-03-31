fe4efead23f3c2aa09a91b764380ca3b6ac5d666
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-31 20:21:07 +0800

    uploaded zfill_alternative.py and added initial pseudocode

---
+++ zfill_alternative.py
@@ -0,0 +1,5 @@
+#define zfill_alt(string: str, len: int) -> str: 
+#initialize new_string = string
+#create while loop to compare len(new_string) with len
+#inside while loop, add "0" to new_string
+#outside while loop, return new_string
