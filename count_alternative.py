838c7c33b0439295931318e42e4e862799d33591
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-04-01 10:02:54 +0800

    added code to program 8 batch 7

--- count_alternative.py
+++ count_alternative.py
@@ -1,8 +1,37 @@
 #define count_alt(string: str, value: str, start: int, end: int) -> int:
-#initialize final_count, compare_value
-#create a for loop to iterate through string according to parameters
-#within for loop, create if statement that evaluates whether i is in value
-#if above statement is True, add i to compare_value and set another if statement that evaluates whether compare_value == value
-#if nested if statement is True, add 1 to final_count
-#outside nested if statement, continue iteration
-#outside initial if, continue else (for characters not included in value), if True, reset compare_value and continue iteration
+def count_alt(string: str, value: str, start: int = 0, end: int = 999999) -> int:
+
+    #initialize final_count, compare_value
+    final_count = 0
+    compare_value = ""
+
+    #create a for loop to iterate through string according to parameters
+    for i in string[start:end:1]:
+
+        #within for loop, create if statement that evaluates whether i is in value
+        if i in value:
+
+            #if above statement is True, add i to compare_value and set another if statement that evaluates whether compare_value == value
+            compare_value += i
+
+            if compare_value == value:
+
+                #if nested if statement is True, add 1 to final_count
+                compare_value = ""
+                final_count += 1
+
+            #outside nested if statement, continue iteration
+            continue
+        
+    #outside initial if, continue else (for characters not included in value), if True, reset compare_value and continue iteration
+        else:
+            compare_value = ""
+            continue
+    
+    return final_count
+
+#tester
+user_string = input("input a string: ")
+user_value = input("input value to count: ")
+
+print(count_alt(user_string, user_value))
