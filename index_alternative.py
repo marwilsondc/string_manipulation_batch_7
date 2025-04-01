c41d7953aa8d196029cfb12f02752d43df17d07b
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-04-01 12:02:03 +0800

    redid code and enhanced code to program 9 batch 7

--- index_alternative.py
+++ index_alternative.py
@@ -1,44 +1,38 @@
 #define index_alt(string: str, val: str, start: int = 0, end: int = 999999) -> int:
 def index_alt(string: str, val: str, start: int = 0, end: int = 999999) -> int:
-
-    #initialize index_count, pause_count, compare_val
     index_count = 0
     pause_count = 0
     compare_val = ""
 
-    #under function, create for loop to iterate through string according to parameters
+    #inside function, create for loop to iterate through string according to parameters
     for i in string[start:end:1]:
 
-        #within for loop, create if statement to evaluate whether i is in val
-        if i in val:
-            
-            #if above statement is True, pass through another if statement that evaluates whether val starts with compare_val
-            if val.startswith(compare_val + i):
-
-                #if nested if statement is True, another if statement that evaluates whether compare_val == val
-                if compare_val == val:
-
-                    #if above nested statement is True, return index_count
-                    return index_count
-                
-                #outside nested statement, continue to else, add 1 to pause_count
-                else:
-                    pause_count += 1
-                
-                continue
-            
-            #else, reset compare_val
-            else:
-                compare_val = ""
+        #inside for loop, create if statement that evaluates whether val starts with compare_val
+        if val.startswith(compare_val + i):
 
             #if above statement is True, add i to compare_val
-            compare_val += i 
-            
+            compare_val += i
 
-        #outside initial if statement, continue to else, reset compare_val and add pause_count to index_count, reset pause_count, and add 1 to index_count
+            #if above statement is True, pass through another if statement that evaluates whether compare_val is equal to val, if True, return index_count
+            if compare_val == val:
+                return index_count
+            
+            #outside nested if statement, continue to else, if True, add 1 to pause_count
+            else:
+                pause_count += 1
+            
+            continue
+        
+        #outside initial if statement, continue to else.
         else:
+            
+            #if else is True, pass through another if statement that evaluates whether pause_count has a value. If True, add pause_count - 1 to index_count
+            if pause_count:
+                index_count += pause_count - 1
+
+            #if else is True, reset compare_val, add new iteration to it, reset pause_count, and add 1 to index_count
             compare_val = ""
-            index_count += pause_count
+            compare_val += i
             pause_count = 0
             index_count += 1
             continue
