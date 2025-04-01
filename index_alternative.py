b92db9926184f550e429c1d8f16ba4578d365278
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-04-01 11:12:01 +0800

    added initial code to program 9 batch 7 and encountered bug: returning None

--- index_alternative.py
+++ index_alternative.py
@@ -11,20 +11,29 @@ def index_alt(string: str, val: str, start: int = 0, end: int = 999999) -> int:
 
         #within for loop, create if statement to evaluate whether i is in val
         if i in val:
-
-            #if above statement is True, add i to compare_val and pass through another if statement to evaluate whether compare_val == val
-            compare_val += i
-
-            if compare_val == val:
-
-                #if nested statement is True, return index_count
-                return index_count
             
-            #outside nested statement, continue to else, add 1 to pause_count
+            #if above statement is True, pass through another if statement that evaluates whether val starts with compare_val
+            if val.startswith(compare_val + i):
+
+                #if nested if statement is True, another if statement that evaluates whether compare_val == val
+                if compare_val == val:
+
+                    #if above nested statement is True, return index_count
+                    return index_count
+                
+                #outside nested statement, continue to else, add 1 to pause_count
+                else:
+                    pause_count += 1
+                
+                continue
+            
+            #else, reset compare_val
             else:
-                pause_count += 1
+                compare_val = ""
+
+            #if above statement is True, add i to compare_val
+            compare_val += i 
             
-            continue
 
         #outside initial if statement, continue to else, reset compare_val and add pause_count to index_count, reset pause_count, and add 1 to index_count
         else:
@@ -34,3 +43,9 @@ def index_alt(string: str, val: str, start: int = 0, end: int = 999999) -> int:
             index_count += 1
             continue
     
+#tester
+user_string = input("input a string: ")
+user_val = input("input a value to evaluate string with: ")
+
+print(index_alt(user_string, user_val))
+print(user_string.index(input("input a value to evaluate string with: ")))
